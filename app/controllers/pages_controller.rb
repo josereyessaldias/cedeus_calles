ded5c4d357fb782d1534c900dd69bc016b4954f4
josereyessaldias <jpreyess@uc.cl>
2020-02-28 12:08:41 -0300

    refinando estilo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,20 +3,23 @@ class PagesController < ApplicationController
   def ditl
   	@ditl_people = Person.where(ditl_check: "ditl_planta")
   	@products = Product.where(ditl_check: true)
-  	@paper_wos = @products.where(tipo: "revista_isi")
+  	@paper_wos = @products.where(tipo: "revista_isi").order(registro: :desc)
   end
 
   def ditl_show
   	@person = Person.find(params[:id])
-  	@person_tipos = []
-		Product.where(ditl_check: true).tipos.keys.each do |type|
-			@person.products.where(ditl_check: true).each do |product|
+    @person_products = @person.products.where(ditl_check: true).order(registro: :desc)
+  	
+    @person_tipos = []
+		Product.tipos.keys.each do |type|
+			@person_products.each do |product|
 				if product.tipo == type
 					@person_tipos << type
 				end
 			end
 		end
 		@person_tipos = @person_tipos.uniq
+
   end
 
 
